b2690009183d630039d22b556c38e07df5b497f8
Jonatan Eduardo Mamani <jonny.m722@gmail.com>
2023-08-02 21:27:32 -0300

    generando cambios j-app.js

--- j-app.js
+++ j-app.js
@@ -1,3 +1,5 @@
 const Bob = {name: "Bob", skills:["cook", "play guitar", "swim"]}
 console.log("Este es un cambio para el conflicto")
-console.log(Bob.name)
+console.log(Bob.Fulanito)
+
+console.log("todo problema se puede resolver")
